535c443d080af76375674388745ce3d53a4f5424
Ngoc Doan <ngocdoan241@gmail.com>
2021-03-14 16:39:15 -0600

    fixing movie app

--- 06-Movie-App/server.js
+++ 06-Movie-App/server.js
@@ -1,100 +1,137 @@
 // Dependencies
 // =============================================================
-
-var express= require("express");
-var path= require=("path");
-
+var express = require("express");
+var app = express();
+var path = require("path");
 
 // Sets up the Express App
 // =============================================================
-
-var app= express();
-var PORT= 8080;
+var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
+// for sending POST requests
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Movie  Characters (DATA) in an array as data
+// Movie  Characters (DATA)
 // =============================================================
 var characters = [
-{
+    { 
+        routeName: "harrypotter",
+        name: "HarryPotter",
+        role: "wizard",
+        age: 17,
+        superPower: "magic"
 
-routeName: "harrypotter",
-name: "harrypotter",
-role: "wizard",
-age: 16,
-superPower: "magic"
-
-},
-
-{
-
-    routeName: "spiderman",
-    name: "spiderman",
-    role: "superhero",
-    age: 17,
-    superPower: "speed"
-    
     },
 
-    {
+    { 
+        routeName: "spiderman",
+        name: "SpiderMan",
+        role: "superhero",
+        age: 100,
+        superPower: "superpowers"
 
-        routeName: "elliot",
-        name: "elliot",
-        role: "hot guy",
-        age: 17,
-        superPower: "big man on campus"
-        
-        }, 
+    },
 
-]
+    { 
+        routeName: "kirby",
+        name: "kirby",
+        role: "pacman",
+        age: 40,
+        superPower: "adventure"
+
+    },
+
+    { 
+        routeName: "pacman",
+        name: "PacMan",
+        role: "eat",
+        age: 70,
+        superPower: "eating"
+
+    },
+
+    { 
+        routeName: "meangirls",
+        name: "catie",
+        role: "plastic",
+        age: 18,
+        superPower: "newgirl"
+
+    }
+];
+
 
 
 // Routes
 // =============================================================
 // Basic route that sends the user first to the Pages
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "view.html"))
+});
 
-app.get("/", function(req,res) {
-
-    res.sendFile(Path.join(_dirname, "add,html"));
+app.get("/add", function(req, res){
+    res.sendFile(path.join(__dirname, "add.html"))
 });
 
 
-app.get("/add", function(req,res) {
 
-    res.sendFile(Path.join(_dirname, "add,html"));
-});
 
 
 // Displays all characters
-
-app.get("/api/characters", function(req, res) {
-    return res.json(characters);
+app.get("/api/characters", function(req, res){
+    res.json(characters);
 });
 
+
+
 // Displays a single character, or returns false
+// Returns the parameter of a specified name
+// Grab the selected parameter
+// Goes through the array for the Characters
+// : before our route to target an array
+app.get('/api/:characters', function(req, res){
 
+	var chosen = req.params.characters;
 
-app.get("/api/characters/:character", function(req,res) {
-    var chosen = req.param.character;
-    console.log(chosen);
+	if(chosen){
+		console.log(chosen);
+
+		for (var i=0; i <characters.length; i++){
+
+			if (chosen == characters[i].routeName){
+				res.json(characters[i]);
+				return;
+			}
+		}
+
+		res.json(false);
+	}
+
+	else{
+		res.json(characters);
+	}
 })
 
 
 
+
+
 // Create New Characters - takes in JSON input
-
-app.post
-
-
-df
-
-
-
 // req.body hosts is equal to the JSON post sent from the user
 // This works because of our body parsing middleware
+app.post("/api/characters", function(req, res) {
+    var newCharacter = req.body;
+
+    console.log(newCharacter)
+
+    characters.push(newCharacter);
+
+    res.json(newCharacter);
+});
+
   
 
 
@@ -109,8 +146,6 @@ df
 
 // Starts the server to begin listening
 // =============================================================
-
-
-app.listen(PORT, function(){
-    console.log("Server listening on: http://localhost:" + PORT)
-});
+app.listen(PORT, function() {
+    console.log("App is listening on port: " + PORT);
+})
